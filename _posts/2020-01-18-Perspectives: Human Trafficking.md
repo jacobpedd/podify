db334a523e9522930aff0bbc5e170c205a690c06
jacob <technology@unldancemarathon.com>
2020-01-18 14:36:04 -0600

    episode adding

---
+++ _posts/2020-01-18-Perspectives: Human Trafficking.md
@@ -0,0 +1,9 @@
+---
+layout: null
+title: "Perspectives: Human Trafficking"
+date: Thu, 09 Jan 2020 05:00:00 +0000
+permalink: https://news.wfsu.org/post/perspectives-human-trafficking-2
+podcast_link: https://cpa.ds.npr.org/wfsu/audio/2020/01/ps200109_cleaned.mp3
+podcast_duration: 3019
+podcast_length: 3019
+---
--- newEpisode.py
+++ newEpisode.py
@@ -1,4 +1,5 @@
 import feedparser
+from datetime import date as dt
 
 
 # date:   2019-01-18
@@ -15,5 +16,29 @@ def main():
     print("\n")  # 2 newlines
 
 
+def addPost():
+    title = "Perspectives: Human Trafficking"
+    date = "Thu, 09 Jan 2020 05:00:00 +0000"
+    link = "https://news.wfsu.org/post/perspectives-human-trafficking-2"
+    file = "https://cpa.ds.npr.org/wfsu/audio/2020/01/ps200109_cleaned.mp3"
+    length = "3019"
+    duration = "3019"
+
+    today = dt.today()
+    d1 = today.strftime("%Y-%m-%d")
+    f = open("_posts/{}-{}.md".format(d1, title), "w+")
+    f.write("---\n")
+    f.write("layout: null\n")
+    f.write('title: "{}"\n'.format(title))
+    f.write("date: {}\n".format(date))
+    f.write("permalink: {}\n".format(link))
+    f.write("podcast_link: {}\n".format(file))
+    f.write("podcast_duration: {}\n".format(duration))
+    f.write("podcast_length: {}\n".format(length))
+    f.write("---")
+    f.close()
+
+
+
 if __name__ == "__main__":
-  main()
+  addPost()
