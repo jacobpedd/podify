25c0df0921fb5e5029a0d69bc22b373be09fd9a9
jacob <technology@unldancemarathon.com>
2020-01-18 14:01:40 -0600

    new epside script

--- newEpisode.py
+++ newEpisode.py
@@ -1,14 +1,17 @@
 import feedparser
 
+
+# date:   2019-01-18
+# categories: podcast
+# permalink: https: // atp.fm/episodes/361
+# podcast_link: http: // traffic.libsyn.com/atpfm/atp361.mp3
+# podcast_duration: "02:12:27"
+# podcast_length: 13654375
+
 def main():
     d = feedparser.parse('https://anchor.fm/s/68c27b4/podcast/rss')
     e = d.entries[0]
-    print(e)
     print(e.title)
-    print(e.published)
-    print(e.link)
-    print(e.links[0])
-    print(e.description)
     print("\n")  # 2 newlines
 
 
