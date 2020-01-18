213518a78e46729cc52f52b22bbcb09b33e31d10
jacob <technology@unldancemarathon.com>
2020-01-18 14:44:13 -0600

    episode desc

--- _posts/2020-01-18-Perspectives: Human Trafficking.md
+++ _posts/2020-01-18-Perspectives: Human Trafficking.md
@@ -7,3 +7,4 @@ podcast_link: https://cpa.ds.npr.org/wfsu/audio/2020/01/ps200109_cleaned.mp3
 podcast_duration: 3019
 podcast_length: 3019
 ---
+January is Human Trafficking Awareness Month and multiple local events are planned to spread the word on the pervasiveness of the crime and what can be done to fight it. To discuss the multiple aspects of the issue are: Robin Hassler Thompson, executive director of the Survive and Thrive Advocacy Center; Regina Bernadin with the International Rescue Committee; Michelle Gaines from the Florida Department of Education; and Melissa Wright from Capital City Bank.
--- _site/newEpisode.py
+++ _site/newEpisode.py
@@ -23,6 +23,7 @@ def addPost():
     file = "https://cpa.ds.npr.org/wfsu/audio/2020/01/ps200109_cleaned.mp3"
     length = "3019"
     duration = "3019"
+    description = "January is Human Trafficking Awareness Month and multiple local events are planned to spread the word on the pervasiveness of the crime and what can be done to fight it. To discuss the multiple aspects of the issue are: Robin Hassler Thompson, executive director of the Survive and Thrive Advocacy Center; Regina Bernadin with the International Rescue Committee; Michelle Gaines from the Florida Department of Education; and Melissa Wright from Capital City Bank."
 
     today = dt.today()
     d1 = today.strftime("%Y-%m-%d")
@@ -35,7 +36,8 @@ def addPost():
     f.write("podcast_link: {}\n".format(file))
     f.write("podcast_duration: {}\n".format(duration))
     f.write("podcast_length: {}\n".format(length))
-    f.write("---")
+    f.write("---\n")
+    f.write(description)
     f.close()
 
 
